977bffef2cbf45b923f0e5dca8d5529b8c6e75d5
Nigless <nigless.gaming@gmail.com>
2022-10-10 20:06:31 +0600

    feat: обновил стили

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,16 +4,24 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { createGlobalStyle } from 'styled-components';
 
 import store from './store';
 import App from './App';
 
 const theme = createTheme();
 
+const Styles = createGlobalStyle`
+*:focus {
+  outline: 4px solid #54baff;
+}
+`;
+
 render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <CssBaseline />
+      <Styles />
       <App />
     </Provider>
   </ThemeProvider>,
